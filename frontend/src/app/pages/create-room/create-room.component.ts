163966986d10ca34c3e4f5d72ad33249b9a3b101
Putt <57494734+PuttTim@users.noreply.github.com>
2022-07-15 18:14:11 +0800

    Recreated Create-Room page, forms are half done.

--- frontend/src/app/pages/create-room/create-room.component.ts
+++ frontend/src/app/pages/create-room/create-room.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-create-room',
-  templateUrl: './create-room.component.html',
-  styleUrls: ['./create-room.component.css']
+    selector: 'app-create-room',
+    templateUrl: './create-room.component.html',
+    styleUrls: ['./create-room.component.css']
 })
 export class CreateRoomComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
--- frontend/src/primeng.module.ts
+++ frontend/src/primeng.module.ts
@@ -3,8 +3,9 @@ import { ButtonModule } from 'primeng/button'
 import { ImageModule } from 'primeng/image'
 import { DividerModule } from 'primeng/divider'
 import { InputTextModule } from 'primeng/inputtext'
+import { StepsModule } from 'primeng/steps';
 
 @NgModule({
-    exports: [ButtonModule, ImageModule, DividerModule, InputTextModule]
+    exports: [ButtonModule, ImageModule, DividerModule, InputTextModule, StepsModule]
 })
 export class PrimeNgModule {}
