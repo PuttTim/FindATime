8812169f4134b2a6633fcee70a4dc66a1a73992b
PuttTim <yannaputt@gmail.com>
2022-08-12 08:18:24 +0800

    BE: Added get user by username and added delete user by id

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const argon2 = require('argon2')
+const { request } = require('express')
 const ObjectId = require('mongodb').ObjectId
 const dbConnection = require('../db')
 
@@ -9,39 +10,53 @@ const argonOptions = {
 
 const db = dbConnection.collection('users')
 
+function getUserById(req, res) {
+    console.log(req.params)
+    db.findOne({ username: req.params.username }, (err, results) => {
+        try {
+            if (results) {
+                // 200: User found
+                const user = {
+                    _id: `${results._id}`,
+                    username: results.username,
+                    tier: results.tier
+                }
+                res.status(200).json(user)
+            } else {
+                // 404: User not found in the collection.
+                res.status(404).json({ message: 'User not found' })
+            }
+        } catch (err) {
+            res.status(500).json({ message: 'Internal server error' })
+        }
+    })
+}
+
 async function createUser(req, res) {
     const user = {
         username: req.body.username,
         email: req.body.email,
-        password: await argon2.hash(req.body.password, argonOptions)
+        password: await argon2.hash(req.body.password, argonOptions),
+        tier: req.body.tier
     }
 
-    db.findOne(
-        { $or: [{ email: user.email }, { username: user.username }] },
-        (err, results) => {
-            try {
-                if (results) {
-                    // 409: Duplicate username or email found.
-                    res.status(409).send('Email or Username already exists')
-                } else {
-                    db.insertOne(user, (err, results) => {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            // 200: User created and inserted successfully
-                            res.status(200).json({
-                                message: 'User created',
-                                _id: `${results.insertedId}`
-                            })
-                            console.log(results)
-                        }
-                    })
-                }
-            } catch (err) {
-                res.status(500).json({ message: 'Internal server error' })
+    db.insertOne(user, (err, results) => {
+        try {
+            if (err.code == 11000) {
+                // 409: Duplicate username or email found.
+                res.status(409).send('Email or Username already exists')
+            } else {
+                // 200: User created and inserted successfully
+                res.status(200).json({
+                    message: 'User created',
+                    _id: `${results.insertedId}`
+                })
             }
+        } catch (err) {
+            // 500: Internal server error
+            res.status(500).json({ message: 'Internal server error' })
         }
-    )
+    })
 }
 
 async function authenticateUser(req, res) {
@@ -53,8 +68,6 @@ async function authenticateUser(req, res) {
     db.findOne({ email: user.email }, async (err, results) => {
         try {
             if (results) {
-                console.log(results)
-
                 const isValid = await argon2.verify(
                     results.password,
                     user.password
@@ -84,12 +97,9 @@ function deleteUser(req, res) {
     console.log(req.body)
     db.deleteOne({ _id: ObjectId(_id) }, (err, results) => {
         try {
-            if (err) {
-                console.log(err)
-            } else if (results.deletedCount == 1) {
+            if (results.deletedCount == 1) {
                 // 200: User successfully deleted
                 res.status(200).json({ message: 'User deleted' })
-                console.log(results)
             } else {
                 // 404: User not found in the collection.
                 res.status(404).json({ message: 'User not found' })
@@ -100,8 +110,38 @@ function deleteUser(req, res) {
     })
 }
 
+function updateUser(req, res) {
+    // Strips the _id from the request body when creating the user object,
+    // and places the _id from the request body into it's own variable.
+    const { _id, ...user } = req.body
+
+    db.updateOne(
+        { _id: ObjectId(_id) },
+        { $set: user },
+        { upsert: true },
+        (err, results) => {
+            try {
+                if ((err.code = 11000)) {
+                    // 409: Duplicate username or email found.
+                    res.status(409).send('Email or Username already exists')
+                } else if (results.modifiedCount == 1) {
+                    // 200: User successfully updated
+                    res.status(200).json({ message: 'User updated' })
+                } else {
+                    // 404: User not found in the collection.
+                    res.status(404).json({ message: 'User not found' })
+                }
+            } catch (err) {
+                res.status(500).json({ message: 'Internal server error' })
+            }
+        }
+    )
+}
+
 module.exports = {
+    getUserById,
     createUser,
     authenticateUser,
-    deleteUser
+    deleteUser,
+    updateUser
 }
--- backend/routes/auth-router.js
+++ backend/routes/auth-router.js
@@ -3,8 +3,10 @@ const authController = require('../controllers/auth-controller')
 
 const router = express.Router()
 
+router.get('/user/:username', authController.getUserById)
 router.post('/register', authController.createUser)
 router.post('/login', authController.authenticateUser)
 router.delete('/delete', authController.deleteUser)
+router.put('/update', authController.updateUser)
 
 module.exports = router
